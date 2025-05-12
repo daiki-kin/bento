ad73cb2041332c547e309ec13a9322e1316c3c40
daiki-kin <daikikinjou344@gmail.com>
2025-05-12 13:47:56 +0900

    デプロイ2

--- config/importmap.rb
+++
@@ -1,7 +0,0 @@
-# Pin npm packages by running ./bin/importmap
-
-pin "application"
-pin "controllers", preload: true
-pin "@hotwired/stimulus", to: "https://cdn.skypack.dev/@hotwired/stimulus"
-pin "@hotwired/stimulus-loading", to: "https://cdn.skypack.dev/@hotwired/stimulus-loading"
-pin_all_from "app/javascript/controllers", under: "controllers"
