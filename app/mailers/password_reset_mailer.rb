59c48d355286cb4c854a276091549d5260b3b0c6
daiki-kin <daikikinjou344@gmail.com>
2025-05-29 18:20:13 +0900

    パスワード再制作

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -1,7 +1,7 @@
 class PasswordResetMailer < ApplicationMailer
     def reset_email
-        @user = params[:user]
-        @url = edit_password_reset_url(token: @user.reset_password_token)
-        mail(to: @user.email, subject: "パスワード再設定のご案内")
+      @user = params[:user]
+      @url = edit_password_reset_url(token: @user.reset_password_token)
+      mail(to: @user.email, subject: "パスワード再設定のご案内")
     end
-end
+  end
