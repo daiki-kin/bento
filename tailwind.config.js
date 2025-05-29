590c7269b2a518dbcd7686fa047f4526e8477121
daiki-kin <daikikinjou344@gmail.com>
2025-05-30 02:20:13 +0900

    新規投稿修正4

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
     "./app/views/**/*.{html.erb,html.haml,html.slim}",
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
+    "./app/components/**/*.{html.erb,html.haml,html.slim}", // 使っていれば追加
+    "./app/assets/stylesheets/**/*.css",
+    "./config/initializers/**/*.rb",
   ],
-  safelist: ["text-red-500", "text-blue-500", "text-green-500"],
+  safelist: [
+    "text-red-500",
+    "text-blue-500",
+    "text-green-500",
+    "mask-star-2",
+    "bg-yellow-400",
+    "text-yellow-400",
+    "rating",
+    "rating-lg", // daisyUI用クラス
+  ],
   theme: {
     extend: {},
   },
